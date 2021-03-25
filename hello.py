b11620f8bc5e5a0c2e2df0e2d7241ce82aff5d63
Stephen Jaeb <sdjaeb@gmail.com>
2021-03-24 20:50:27 -0500

    Set up sample json file and read from it

--- hello.py
+++ hello.py
@@ -1,10 +1,12 @@
 from flask import Flask
 from flask import request
 
+import httpx
+import json
+
 app = Flask(__name__)
 
-''' http://lookup-service-prod.mlb.com/json/named.search_player_all.bam?sport_code=%27mlb%27&active_sw=%27Y%27&name_part=%27pu%25%27
-'''
+
 @app.route('/')
 def index():
     return 'Index Page'
@@ -13,6 +15,38 @@ def index():
 def hello_world():
     return 'Hello, World!'
 
-@app.route('/stats')
-def get_stats():
-	return 'Stats!'
+'''
+	Eventually pass in filter (partial name, positions, team, etc)
+'''
+@app.route('/api-stats')
+def get_stats_from_api():
+	'''
+		http://lookup-service-prod.mlb.com/json/named.search_player_all.bam?sport_code=%27mlb%27&active_sw=%27Y%27&name_part=%27pu%25%27
+		Only return:
+			full name,
+			full team name,
+			player id,
+			weight,
+			height (ft' in"),
+			bats/throws,
+			position
+	'''
+	return 'Stats from API'
+
+@app.route('/json-stats')
+def get_stats_from_json():
+	'''
+		Load player_stats.json
+		Only return:
+			full name,
+			full team name,
+			player id,
+			weight,
+			height (ft' in"),
+			bats/throws,
+			position
+	'''
+	f = open("mlbplayers.json",)
+	players = json.load(f)
+
+	return players
