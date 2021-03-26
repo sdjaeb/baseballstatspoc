bc584b790398b5c1bb624dd808211a5706ab3edb
Stephen Jaeb <sdjaeb@gmail.com>
2021-03-26 09:15:13 -0500

    Add some exception handling.  The pattern: print is a placeholder for triggering some logging mechanism, if any, and the return after it is to trigger some sort of component or technical resource to do something.

--- player_info_stats.py
+++ player_info_stats.py
@@ -6,7 +6,7 @@ import json
 
 app = Flask(__name__)
 
-def normalize_player_info(player_list, field_names, N=10):
+def normalize_player_info(player_list, field_names, N=10) -> list:
 	"""Preps the data from disparate data sources to use a common schema"""
 	players = []
 	fieldNames = [
@@ -22,25 +22,40 @@ def normalize_player_info(player_list, field_names, N=10):
 	]
 
 	# pluck the fields from the player list specified by the field_names
-	for playerInfo in player_list:
-		# filter out the data we don't need
-		filteredPlayerInfo = { key: playerInfo[key] for key in field_names }
+	try:
+		for playerInfo in player_list:
+			# filter out the data we don't need
+			try:
+				filteredPlayerInfo = { key: playerInfo[key] for key in field_names }
+			except KeyError:
+				print('KeyError - normalize_player_info - missing key in playerInfo loop')
+				return 'KeyError - normalize_player_info - missing key in playerInfo loop'
+			except NameError:
+				print('NameError - normalize_player_info - name error in playerInfo loop')
+				return 'NameError - normalize_player_info - name error in playerInfo loop'
 
-		# normalize the field names
-		filteredAndNormalizedPlayerInfo = {}
-		for k, v in filteredPlayerInfo.items():
-			normalizedFieldName = fieldNames[field_names.index(k)]
-			filteredAndNormalizedPlayerInfo[normalizedFieldName] = v
+			# normalize the field names
+			filteredAndNormalizedPlayerInfo = {}
+			for k, v in filteredPlayerInfo.items():
+				normalizedFieldName = fieldNames[field_names.index(k)]
+				filteredAndNormalizedPlayerInfo[normalizedFieldName] = v
 
-		players.append(filteredAndNormalizedPlayerInfo)
-
-	# return the first N players
-	return players[:N]
+			players.append(filteredAndNormalizedPlayerInfo)
+	except NameError:
+		print('NameError - player_list for loop')
+		return 'NameError - player_list for loop'
+	else:
+		# return the first N players
+		return players[:N]
 
 def get_stats_from_api() -> list:
 	"""Gets player info and stats from an api"""
 	url = "http://lookup-service-prod.mlb.com/json/named.search_player_all.bam?sport_code=%27mlb%27&active_sw=%27Y%27&name_part=%27a%25%27"
-	response = requests.get(url).json()
+	try:
+		response = requests.get(url).json()
+	except json.decoder.JSONDecodeError:
+		print('JSONDecodeError - get_stats_from_api - There was an error decoding the json from the response')
+		return 'JSONDecodeError - get_stats_from_api - There was an error decoding the json from the response'
 
 	fieldsToPluck = [
 		'position',
@@ -54,12 +69,30 @@ def get_stats_from_api() -> list:
 		'name_last'
 	]
 
-	return normalize_player_info(response['search_player_all']['queryResults']['row'], fieldsToPluck, 10)
+	try:
+		normalizedPlayerInfo = normalize_player_info(response['search_player_all']['queryResults']['row'], fieldsToPluck, 10)
+	except KeyError:
+		print('KeyError - get_stats_from_api - issue with key in normalize_player_info')
+		return 'KeyError - get_stats_from_api - issue with key in normalize_player_info'
+	except NameError:
+		print('NameError - get_stats_from_api - issue with a name near normalize_player_info')
+		return 'NameError - get_stats_from_api - issue with a name near normalize_player_info'
+
+	return normalizedPlayerInfo
 
 def get_stats_from_json() -> list:
 	"""Gets player info and stats from a json file"""
-	f = open("mlbplayers.json",)
-	rawPlayerData = json.load(f)
+	try:
+		f = open('mlbplayers.json',)
+	except FileNotFoundError:
+		print('FileNotFoundError - get_stats_from_json')
+		return 'FileNotFoundError - get_stats_from_json'
+
+	try:
+		rawPlayerData = json.load(f)
+	except NameError:
+		print('NameError - get_stats_from_json loading rawPlayerData')
+		return 'NameError - get_stats_from_json loading rawPlayerData'
 
 	fieldsToPluck = [
 		'position',
@@ -73,7 +106,16 @@ def get_stats_from_json() -> list:
 		'lastName'
 	]
 
-	return normalize_player_info(rawPlayerData['players'], fieldsToPluck, 10)
+	try:
+		normalizedPlayerInfo = normalize_player_info(rawPlayerData['players'], fieldsToPluck, 10)
+	except KeyError:
+		print('KeyError - get_stats_from_json - Invalid key in rawPlayerData')
+		return 'KeyError - get_stats_from_json - Invalid key in rawPlayerData'
+	except NameError:
+		print('NameError - get_stats_from_json - something happened assigning normalizedPlayerInfo')
+		return 'NameError - get_stats_from_json - something happened assigning normalizedPlayerInfo'
+	else:
+		return normalizedPlayerInfo
 
 @app.route('/')
 @app.route('/combined-stats')
@@ -81,12 +123,48 @@ def get_combined_stats():
 	api_stats = get_stats_from_api()
 	json_stats = get_stats_from_json()
 
-	return render_template('player_list.html', data=(api_stats + json_stats))
+	try:
+		rendered_template = render_template('player_list.html', data=(api_stats + json_stats))
+	except NameError:
+		print('NameError - get_combined_stats')
+		return 'NameError - get_combined_stats'
+	except TypeError:
+		print('TypeError - get_combined_stats')
+		return 'TypeError - get_combined_stats'
+	except:
+		print('Exception - get_combined_stats')
+		return 'Exception - get_combined_stats'
+	else:
+		return rendered_template
 
 @app.route('/api-stats')
 def api_stats():
-	return render_template('player_list.html', data=get_stats_from_api())
+	try:
+		rendered_template = render_template('player_list.html', data=get_stats_from_api1())
+	except NameError:
+		print('NameError - api_stats')
+		return 'NameError - api_stats'
+	except TypeError:
+		print('TypeError - api_stats')
+		return 'TypeError - api_stats'
+	except:
+		print('Exception - api_stats')
+		return 'Exception - api_stats'
+	else:
+		return rendered_template
 
 @app.route('/json-stats')
 def json_stats():
-	return render_template('player_list.html', data=get_stats_from_json())
+	try:
+		rendered_template = render_template('player_list.html1', data=get_stats_from_json())
+	except NameError:
+		print('NameError - json_stats')
+		return 'NameError - json_stats'
+	except TypeError:
+		print('TypeError - json_stats')
+		return 'TypeError - json_stats'
+	except:
+		print('Exception - json_stats')
+		return 'Exception - json_stats'
+	else:
+		return rendered_template
